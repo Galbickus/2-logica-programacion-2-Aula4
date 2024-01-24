d50965e1b8c4ac36c1d1876e1676b8b7cdf75569
Veronica <135274833+Galbickus@users.noreply.github.com>
2024-01-24 03:36:21 -0300

    ej 6 y 7

--- desafioParte2Clase4.js
+++ desafioParte2Clase4.js
@@ -65,3 +65,55 @@ function muestraArrayAlReves(){
 
 //llamada a la f(x)
 muestraArrayAlReves();
+
+//6.  Crea una función que calcule el promedio de los elementos en una lista de números.
+
+let listaDeNumeros = [1,2,3,4,5,6,7,8,9];
+let suma = 0;
+
+function promediar(){
+    for (let i = 0; i < listaDeNumeros.length; i++) {
+        suma = suma + listaDeNumeros[i];
+    }
+    let promedio = suma/listaDeNumeros.length;
+    return promedio;
+};
+
+//llamada a la f(x)
+let resultado = promediar();
+console.log(resultado);
+
+//7.  Crea una función que muestre en la consola el número más grande
+// y el número más pequeño en una lista.
+
+// Declaración de la función llamada encontrarMayorMenor que toma un parámetro llamado lista
+function encontrarMayorMenor(lista) {
+    // Inicialización de las variables mayor y menor con el primer elemento de la lista
+    let mayor = lista[0];
+    let menor = lista[0];
+  
+    // Bucle que comienza en el segundo elemento de la lista (índice 1)
+    for (let i = 1; i < lista.length; i++) {
+        // Comprueba si el elemento actual es mayor que el valor almacenado en la variable mayor
+        if (lista[i] > mayor) {
+            // Si es así, actualiza la variable mayor con el valor actual del elemento
+            mayor = lista[i];
+        }
+        // Comprueba si el elemento actual es menor que el valor almacenado en la variable menor
+        if (lista[i] < menor) {
+            // Si es así, actualiza la variable menor con el valor actual del elemento
+            menor = lista[i];
+        }
+    }
+  
+    // Imprime en la consola el valor almacenado en la variable mayor
+    console.log('Mayor:', mayor);
+    // Imprime en la consola el valor almacenado en la variable menor
+    console.log('Menor:', menor);
+}
+
+// Declaración de un array llamado numeros con algunos valores
+let numeros = [15, 8, 25, 5, 12];
+
+// Llamada a la función encontrarMayorMenor pasando el array numeros como argumento
+encontrarMayorMenor(numeros);
