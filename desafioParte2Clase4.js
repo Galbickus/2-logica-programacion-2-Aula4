f79ea3b101ce4ba292e700cddb8a4a561274edf7
Veronica <135274833+Galbickus@users.noreply.github.com>
2024-01-24 02:49:20 -0300

    hasta  ej 5

--- desafioParte2Clase4.js
+++ desafioParte2Clase4.js
@@ -14,3 +14,54 @@ Desafíos
 9.  Crea una función que devuelva la posición en la lista donde se encuentra un elemento pasado como parámetro, o -1 si no existe en la lista.
 10. Crea una función que reciba dos listas de números del mismo tamaño y devuelva una nueva lista con la suma de los elementos uno a uno.
 11. Crea una función que reciba una lista de números y devuelva una nueva lista con el cuadrado de cada número. */
+
+//1.  Crea una lista vacía llamada "listaGenerica".
+
+let listaGenerica = [];
+
+console.log(listaGenerica);
+
+//2.  Crea una lista de lenguajes de programación llamada
+// "lenguagesDeProgramacion con los siguientes 
+//elementos: 'JavaScript', 'C', 'C++', 'Kotlin' y 'Python'.
+
+let lenguagesDeProgramacion = ["JavaScript", "C", "C++", "Kotlin", "Python"];
+console.log(lenguagesDeProgramacion);
+
+//3.  Agrega a la lista "lenguagesDeProgramacion los siguientes
+// elementos: 'Java', 'Ruby' y 'GoLang'.
+
+lenguagesDeProgramacion.push("Java");
+console.log(lenguagesDeProgramacion);
+
+lenguagesDeProgramacion.push("Ruby");
+console.log(lenguagesDeProgramacion);
+
+lenguagesDeProgramacion.push("GoLang");
+console.log(lenguagesDeProgramacion);
+
+//4.  Crea una función que muestre en la consola todos los
+// elementos de la lista "lenguagesDeProgramacion.
+
+function muestraArray(){
+
+    for (let i = 0; i < lenguagesDeProgramacion.length; i++) {
+        console.log(lenguagesDeProgramacion[i]);
+    }
+};
+
+//llamada a la f(x)
+muestraArray();
+
+//5.  Crea una función que muestre en la consola todos los elementos de la lista
+// "lenguagesDeProgramacion en orden inverso.
+
+function muestraArrayAlReves(){
+
+    for (let i = lenguagesDeProgramacion.length-1; i>=0; i--) {
+        console.log(lenguagesDeProgramacion[i]);
+    }
+};
+
+//llamada a la f(x)
+muestraArrayAlReves();
